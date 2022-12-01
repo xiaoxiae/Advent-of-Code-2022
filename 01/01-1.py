191f47fefe4a83d1a600f799356a819e6389d8fc
xiaoxiae <tomas@slama.dev>
2022-12-01 12:28:30 +0100

    init + day 1

---
+++ 01/01-1.py
@@ -0,0 +1,12 @@
+import sys
+
+sys.path.insert(0, "../")
+from utilities import success, get_input
+
+input = get_input(whole=True)
+
+groups = []
+for group in input.split("\n\n"):
+    groups.append(sum(map(int, group.splitlines())))
+
+success(max(groups))
---
+++ README.md
@@ -0,0 +1,2 @@
+# Advent of Code 2022
+A repository containing my solutions and inputs for the [2022 Advent of Code](https://adventofcode.com/2022) problems.
