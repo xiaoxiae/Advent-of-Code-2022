cd7a98ed464aeaf36a4f3bcbd6334198b9c4cebb
xiaoxiae <tomas@slama.dev>
2022-12-06 10:43:52 +0100

    day 6

---
+++ 06/06-2.py
@@ -0,0 +1,11 @@
+import sys
+
+sys.path.insert(0, "../")
+from utilities import success, get_input
+
+packet = get_input(whole=True)
+
+w = 14
+for i in range(len(packet) - w):
+    if len(set(packet[i:i+w])) == len(packet[i:i+w]):
+        success(i+w)
